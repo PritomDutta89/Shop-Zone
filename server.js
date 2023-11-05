82338b1f6d65a32bc36f1ffcd5012441783432be
PRITOM DUTTA <duttapritom777@gmail.com>
2023-11-05 23:07:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from "cors";
 import path from 'path';
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
@@ -18,6 +19,10 @@ dotenv.config();
 
 //database config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
